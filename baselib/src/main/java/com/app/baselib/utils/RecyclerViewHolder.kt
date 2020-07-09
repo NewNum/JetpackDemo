2004b780a30b1121f88e7c7d1e3bcce2d7eb4ac1
Huxh <huxinhui1@163.com>
2020-07-09 14:25:11 +0800

    优化viewholder

--- baselib/src/main/java/com/app/baselib/utils/RecyclerViewHolder.kt
+++ baselib/src/main/java/com/app/baselib/utils/RecyclerViewHolder.kt
@@ -1,23 +1,18 @@
 package com.app.baselib.utils
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import java.util.*
 
 /**
- * @author zongjin
+ * @author huxh
  * @date 2019/3/23.
  */
-class RecyclerViewHolder private constructor(val context: Context, val convertView: View) :
+class RecyclerViewHolder private constructor(convertView: View) :
     RecyclerView.ViewHolder(convertView), LayoutContainer {
     private val mViews: SparseArray<View> by lazy { SparseArray<View>() }
     private val objects: HashMap<String, Any> by lazy { HashMap<String, Any>() }
@@ -29,7 +24,7 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
         return if (objects[key] == null) {
             null
         } else {
-            objects[key] as T?
+            objects[key] as? T
         }
     }
 
@@ -43,13 +38,13 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
      * @param viewId
      * @return
      */
-    fun <T : View> getView(viewId: Int): T {
+    fun <T : View> getView(viewId: Int): T? {
         var view: View? = mViews.get(viewId)
         if (view == null) {
-            view = convertView.findViewById(viewId)
+            view = itemView.findViewById(viewId)
             mViews.put(viewId, view)
         }
-        return view as T
+        return view as? T
     }
 
     /**
@@ -57,13 +52,13 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
      *
      * @return
      */
-    fun <T : View> getView(viewId: Int, clazz: Class<T>): T {
+    fun <T : View> getView(viewId: Int, clazz: Class<T>): T? {
         var view: View? = mViews.get(viewId)
         if (view == null) {
-            view = convertView.findViewById(viewId)
+            view = itemView.findViewById(viewId)
             mViews.put(viewId, view)
         }
-        return view as T
+        return view as? T
     }
 
     /**
@@ -72,7 +67,7 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
      * @param viewId
      * @return
      */
-    fun <T : View> findViewById(viewId: Int): T {
+    fun <T : View> findViewById(viewId: Int): T? {
         return getView(viewId)
     }
 
@@ -81,47 +76,8 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
      *
      * @return
      */
-    fun <T : View> findViewById(viewId: Int, clazz: Class<T>): T {
+    fun <T : View> findViewById(viewId: Int, clazz: Class<T>): T? {
         return getView(viewId, clazz)
-    }
-
-
-    /****以下为辅助方法 */
-
-    /**
-     * 设置TextView的值
-     *
-     * @param viewId
-     * @param text
-     * @return
-     */
-    fun setText(viewId: Int, text: String?): RecyclerViewHolder {
-        val tv = getView<TextView>(viewId)
-        tv.text = text
-        return this
-    }
-
-    fun setImageResource(viewId: Int, resId: Int): RecyclerViewHolder {
-        val view = getView<ImageView>(viewId)
-        view.setImageResource(resId)
-        return this
-    }
-
-    /*   fun setImageUrl(viewId: Int, url: String): RecyclerViewHolder {
-           Glide.with(context).load(url).into(getView<View>(viewId) as ImageView)
-           return this
-       }*/
-
-    fun setImageBitmap(viewId: Int, bitmap: Bitmap): RecyclerViewHolder {
-        val view = getView<ImageView>(viewId)
-        view.setImageBitmap(bitmap)
-        return this
-    }
-
-    fun setImageDrawable(viewId: Int, drawable: Drawable): RecyclerViewHolder {
-        val view = getView<ImageView>(viewId)
-        view.setImageDrawable(drawable)
-        return this
     }
 
     /**
@@ -132,7 +88,7 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
         listener: View.OnClickListener
     ): RecyclerViewHolder {
         val view = getView<View>(viewId)
-        view.setOnClickListener(listener)
+        view?.setOnClickListener(listener)
         return this
     }
 
@@ -142,23 +98,16 @@ class RecyclerViewHolder private constructor(val context: Context, val convertVi
 
     companion object {
 
-        fun createViewHolder(context: Context, itemView: View): RecyclerViewHolder {
-            return RecyclerViewHolder(context, itemView)
-        }
-
         fun createViewHolder(parent: ViewGroup, layoutId: Int): RecyclerViewHolder {
-            return createViewHolder(parent.context, parent,layoutId)
+            return createViewHolder(
+                LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+            )
         }
 
         fun createViewHolder(
-            context: Context,
-            parent: ViewGroup, layoutId: Int
+            view: View
         ): RecyclerViewHolder {
-            val itemView = LayoutInflater.from(context).inflate(
-                layoutId, parent,
-                false
-            )
-            return RecyclerViewHolder(context, itemView)
+            return RecyclerViewHolder(view)
         }
     }
 
