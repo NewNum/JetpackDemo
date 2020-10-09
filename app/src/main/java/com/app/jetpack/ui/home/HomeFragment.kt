87a4fa8561ee271aadf2cb61d98ee2d1a0b02a7a
HuXinhui <xinhui.hu@wkzuche.com>
2020-10-09 19:42:23 +0800

    添加沉浸式

--- app/src/main/java/com/app/jetpack/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/jetpack/ui/home/HomeFragment.kt
@@ -6,19 +6,19 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.jetpack.R
-import com.app.baselib.base.fragment.BaseDataBindingViewModelFragment
-import com.app.jetpack.databinding.FragmentHomeBinding
+import com.app.baselib.base.fragment.BaseViewModelFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : BaseDataBindingViewModelFragment<FragmentHomeBinding>() {
+class HomeFragment : BaseViewModelFragment() {
 
     override val viewModel: HomeArticleViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        db.rvHome.layoutManager = LinearLayoutManager(requireContext())
-        db.rvHome.adapter = HomeArticleAdapter()
+        rvHome.layoutManager = LinearLayoutManager(requireContext())
+        rvHome.adapter = HomeArticleAdapter()
         viewModel.pagedList.observe(viewLifecycleOwner, Observer {
-            val adapter = db.rvHome.adapter
+            val adapter = rvHome.adapter
             if (adapter is HomeArticleAdapter) {
                 adapter.submitList(it)
             }
